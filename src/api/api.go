e911985b4943f4bd6fe6f5f63122603ce64146ac
Saravanan <g.gsaravanan@gmail.com>
2020-04-03 16:35:09 +0000

    api get method and docker added

--- src/api/api.go
+++ src/api/api.go
@@ -8,8 +8,14 @@ import (
 )
 
 func PostMethod(c *gin.Context) {
-  fmt.Println("\napi.go 'postMethod' called")
+  fmt.Println("\napi.go 'PostMethod' called")
   message := "PostMethod called"
+  c.JSON(http.StatusOK, message)
+}
+
+func GetMethod(c *gin.Context) {
+  fmt.Println("\napi.go 'GetMethod' called")
+  message := "GetMethod called"
   c.JSON(http.StatusOK, message)
 }
 
@@ -17,6 +23,7 @@ func main() {
   router := gin.Default()
 
   router.POST("/", PostMethod)
+  router.GET("/", GetMethod)
 
   listenPort := "4000"
   // Listen and Server on the LocalHost:Port
