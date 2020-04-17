4a04ad8b9087474f7a24127c20cf69ded20a75fe
Saravanan <g.gsaravanan@gmail.com>
2020-04-17 13:54:04 +0000

    new copy of api source

---
+++ src/apicopy/api.go
@@ -0,0 +1,31 @@
+// Golang REST API program
+package api
+
+import (
+  "fmt"
+  "net/http"
+  "github.com/gin-gonic/gin"
+)
+
+func PostMethod(c *gin.Context) {
+  fmt.Println("\napi.go 'PostMethod' called")
+  message := "PostMethod called"
+  c.JSON(http.StatusOK, message)
+}
+
+func GetMethod(c *gin.Context) {
+  fmt.Println("\napi.go 'GetMethod' called")
+  message := "GetMethod called"
+  c.JSON(http.StatusOK, message)
+}
+
+func main() {
+  router := gin.Default()
+
+  router.POST("/", PostMethod)
+  router.GET("/", GetMethod)
+
+  listenPort := "4000"
+  // Listen and Server on the LocalHost:Port
+  router.Run(":"+listenPort)
+}
--- src/apicopy/go.mod
+++ src/apicopy/go.mod
@@ -1,4 +1,4 @@
-module apiauth
+module example.com/api
 
 go 1.14
 
