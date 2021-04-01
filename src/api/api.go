98ef6e19935ba3f8f1c95fb24e4ff8d2f1090bb5
Saravanan G <g.gsaravanan@gmail.com>
2021-04-01 12:30:10 +0000

    Put method added in api.go

--- src/api/api.go
+++ src/api/api.go
@@ -19,11 +19,18 @@ func GetMethod(c *gin.Context) {
   c.JSON(http.StatusOK, message)
 }
 
+func PutMethod(c *gin.Context) {
+  fmt.Println("\napi.go 'PutMethod' called")
+  message := "PutMethod called"
+  c.JSON(http.StatusOK, message)
+}
+
 func main() {
   router := gin.Default()
 
   router.POST("/", PostMethod)
   router.GET("/", GetMethod)
+  router.PUT("/", PutMethod)
 
   listenPort := "4000"
   // Listen and Server on the LocalHost:Port
--- src/api/api_test.go
+++ src/api/api_test.go
@@ -72,3 +72,35 @@ func TestGetMethod(t *testing.T) {
         t.Fatalf("Expected to get status %d but instead got %d\n", http.StatusOK, w.Code)
     }
 }
+
+func TestPutMethod(t *testing.T) {
+    // Switch to test mode so you don't get such noisy output
+    gin.SetMode(gin.TestMode)
+
+    // Setup your router, just like you did in your main function, and
+    // register your routes
+    r := gin.Default()
+    r.PUT("/", PutMethod)
+
+    // Create the mock request you'd like to test. Make sure the second argument
+    // here is the same as one of the routes you defined in the router setup
+    // block!
+    req, err := http.NewRequest(http.MethodPut, "/", nil)
+    if err != nil {
+        t.Fatalf("Couldn't create request: %v\n", err)
+    }
+
+    // Create a response recorder so you can inspect the response
+    w := httptest.NewRecorder()
+
+    // Perform the request
+    r.ServeHTTP(w, req)
+    fmt.Println(w.Body)
+
+    // Check to see if the response was what you expected
+    if w.Code == http.StatusOK {
+        t.Logf("Expected to get status %d is same ast %d\n", http.StatusOK, w.Code)
+    } else {
+        t.Fatalf("Expected to get status %d but instead got %d\n", http.StatusOK, w.Code)
+    }
+}
