524aa54a3308001134e20daf232b2fd0e0ddb9d2
Saravanan Gnanaguru <g.gsaravanan@gmail.com>
2021-12-22 16:35:27 +0530

    fixed gin versions

--- src/apiauth/apiauth.go
+++ src/apiauth/apiauth.go
@@ -2,10 +2,12 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "github.com/gin-gonic/gin"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
+
 /*
 func PostMethod(c *gin.Context) {
   fmt.Println("\napi.go 'PostMethod' called")
@@ -14,47 +16,47 @@ func PostMethod(c *gin.Context) {
 }
 */
 func GetMethod(c *gin.Context) {
-  fmt.Println("\n'GetMethod' called")
-  IdValue := c.Params.ByName("IdValue")
-  message := "GetMethod Called With Param: " + IdValue
-  c.JSON(http.StatusOK, message)
+	fmt.Println("\n'GetMethod' called")
+	IdValue := c.Params.ByName("IdValue")
+	message := "GetMethod Called With Param: " + IdValue
+	c.JSON(http.StatusOK, message)
 
-  ReqPayload := make([]byte, 1024)
-  ReqPayload, err := c.GetRawData()
-  if err != nil {
-        fmt.Println(err)
-        return
-  }
-  //fmt.Println("Request Payload Data: ", string(ReqPayload))
+	/*   ReqPayload := make([]byte, 1024)
+	     ReqPayload, err := c.GetRawData()
+	     if err != nil {
+	           fmt.Println(err)
+	           return
+	     }
+	     fmt.Println("Request Payload Data: ", string(ReqPayload)) */
 }
 
 func PostMethod(c *gin.Context) {
-  fmt.Println("\n'PostMethod' called")
-  IdValue := c.Params.ByName("IdValue")
-  message := "PostMethod Called With Param: " + IdValue
-  c.JSON(http.StatusOK, message)
+	fmt.Println("\n'PostMethod' called")
+	IdValue := c.Params.ByName("IdValue")
+	message := "PostMethod Called With Param: " + IdValue
+	c.JSON(http.StatusOK, message)
 
-  ReqPayload := make([]byte, 1024)
-  ReqPayload, err := c.GetRawData()
-  if err != nil {
-        fmt.Println(err)
-        return
-  }
-  //fmt.Println("Request Payload Data: ", string(ReqPayload))
+	/*   ReqPayload := make([]byte, 1024)
+	     ReqPayload, err := c.GetRawData()
+	     if err != nil {
+	           fmt.Println(err)
+	           return
+	     }
+	     fmt.Println("Request Payload Data: ", string(ReqPayload)) */
 }
 
 func main() {
-  router := gin.Default()
+	router := gin.Default()
 
-  // Create Sub Router for  customised API version
-  subRouterAuthenticated := router.Group("/api/v1/PersonId", gin.BasicAuth(gin.Accounts{
-    "admin": "adminpass",
-  }))
+	// Create Sub Router for  customised API version
+	subRouterAuthenticated := router.Group("/api/v1/PersonId", gin.BasicAuth(gin.Accounts{
+		"admin": "adminpass",
+	}))
 
-  subRouterAuthenticated.POST("/:IdValue", PostMethod)
-  subRouterAuthenticated.GET("/:IdValue", GetMethod)
+	subRouterAuthenticated.POST("/:IdValue", PostMethod)
+	subRouterAuthenticated.GET("/:IdValue", GetMethod)
 
-  listenPort := "4000"
-  // Listen and Server on the LocalHost:Port
-  router.Run(":"+listenPort)
+	listenPort := "4000"
+	// Listen and Server on the LocalHost:Port
+	router.Run(":" + listenPort)
 }
