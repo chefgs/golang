5a262f9ff7a7236003d3ca5322db9a1f977c45ec
Saravanan G <g.gsaravanan@gmail.com>
2022-10-25 22:12:46 +0530

    Adding new GHA workflow  (#3)
    * Create Makefile
    
    * Adding changes for new workflow
    
    * added new workflow with bash run block
    
    * added new workflow with bash run block
    
    * added new workflow with bash run block
    
    * added new workflow with bash run block

--- src/api/api.go
+++ src/api/api.go
@@ -2,37 +2,37 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "github.com/gin-gonic/gin"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func PostMethod(c *gin.Context) {
-  fmt.Println("\napi.go 'PostMethod' called")
-  message := "PostMethod called"
-  c.JSON(http.StatusOK, message)
+	fmt.Println("\napi.go 'PostMethod' called")
+	message := "PostMethod called"
+	c.JSON(http.StatusOK, message)
 }
 
 func GetMethod(c *gin.Context) {
-  fmt.Println("\napi.go 'GetMethod' called")
-  message := "GetMethod called"
-  c.JSON(http.StatusOK, message)
+	fmt.Println("\napi.go 'GetMethod' called")
+	message := "GetMethod called"
+	c.JSON(http.StatusOK, message)
 }
 
 func PutMethod(c *gin.Context) {
-  fmt.Println("\napi.go 'PutMethod' called")
-  message := "PutMethod called"
-  c.JSON(http.StatusOK, message)
+	fmt.Println("\napi.go 'PutMethod' called")
+	message := "PutMethod called"
+	c.JSON(http.StatusOK, message)
 }
 
 func main() {
-  router := gin.Default()
+	router := gin.Default()
 
-  router.POST("/", PostMethod)
-  router.GET("/", GetMethod)
-  router.PUT("/", PutMethod)
+	router.POST("/", PostMethod)
+	router.GET("/", GetMethod)
+	router.PUT("/", PutMethod)
 
-  listenPort := "4000"
-  // Listen and Server on the LocalHost:Port
-  router.Run(":"+listenPort)
+	listenPort := "4000"
+	// Listen and Server on the LocalHost:Port
+	router.Run(":" + listenPort)
 }
--- src/api/api_test.go
+++ src/api/api_test.go
@@ -2,105 +2,105 @@
 package main
 
 import (
-  "testing"
-  "net/http"
-  "net/http/httptest"
-  "fmt"
-  "github.com/gin-gonic/gin"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 )
 
 func TestPostMethod(t *testing.T) {
-    // Switch to test mode so you don't get such noisy output
-    gin.SetMode(gin.TestMode)
+	// Switch to test mode so you don't get such noisy output
+	gin.SetMode(gin.TestMode)
 
-    // Setup your router, just like you did in your main function, and
-    // register your routes
-    r := gin.Default()
-    r.POST("/", PostMethod)
+	// Setup your router, just like you did in your main function, and
+	// register your routes
+	r := gin.Default()
+	r.POST("/", PostMethod)
 
-    // Create the mock request you'd like to test. Make sure the second argument
-    // here is the same as one of the routes you defined in the router setup
-    // block!
-    req, err := http.NewRequest(http.MethodPost, "/", nil)
-    if err != nil {
-        t.Fatalf("Couldn't create request: %v\n", err)
-    }
+	// Create the mock request you'd like to test. Make sure the second argument
+	// here is the same as one of the routes you defined in the router setup
+	// block!
+	req, err := http.NewRequest(http.MethodPost, "/", nil)
+	if err != nil {
+		t.Fatalf("Couldn't create request: %v\n", err)
+	}
 
-    // Create a response recorder so you can inspect the response
-    w := httptest.NewRecorder()
+	// Create a response recorder so you can inspect the response
+	w := httptest.NewRecorder()
 
-    // Perform the request
-    r.ServeHTTP(w, req)
-    fmt.Println(w.Body)
+	// Perform the request
+	r.ServeHTTP(w, req)
+	fmt.Println(w.Body)
 
-    // Check to see if the response was what you expected
-    if w.Code == http.StatusOK {
-        t.Logf("Expected to get status %d is same ast %d\n", http.StatusOK, w.Code)
-    } else {
-        t.Fatalf("Expected to get status %d but instead got %d\n", http.StatusOK, w.Code)
-    }
+	// Check to see if the response was what you expected
+	if w.Code == http.StatusOK {
+		t.Logf("Expected to get status %d is same ast %d\n", http.StatusOK, w.Code)
+	} else {
+		t.Fatalf("Expected to get status %d but instead got %d\n", http.StatusOK, w.Code)
+	}
 }
 
 func TestGetMethod(t *testing.T) {
-    // Switch to test mode so you don't get such noisy output
-    gin.SetMode(gin.TestMode)
+	// Switch to test mode so you don't get such noisy output
+	gin.SetMode(gin.TestMode)
 
-    // Setup your router, just like you did in your main function, and
-    // register your routes
-    r := gin.Default()
-    r.GET("/", GetMethod)
+	// Setup your router, just like you did in your main function, and
+	// register your routes
+	r := gin.Default()
+	r.GET("/", GetMethod)
 
-    // Create the mock request you'd like to test. Make sure the second argument
-    // here is the same as one of the routes you defined in the router setup
-    // block!
-    req, err := http.NewRequest(http.MethodGet, "/", nil)
-    if err != nil {
-        t.Fatalf("Couldn't create request: %v\n", err)
-    }
+	// Create the mock request you'd like to test. Make sure the second argument
+	// here is the same as one of the routes you defined in the router setup
+	// block!
+	req, err := http.NewRequest(http.MethodGet, "/", nil)
+	if err != nil {
+		t.Fatalf("Couldn't create request: %v\n", err)
+	}
 
-    // Create a response recorder so you can inspect the response
-    w := httptest.NewRecorder()
+	// Create a response recorder so you can inspect the response
+	w := httptest.NewRecorder()
 
-    // Perform the request
-    r.ServeHTTP(w, req)
-    fmt.Println(w.Body)
+	// Perform the request
+	r.ServeHTTP(w, req)
+	fmt.Println(w.Body)
 
-    // Check to see if the response was what you expected
-    if w.Code == http.StatusOK {
-        t.Logf("Expected to get status %d is same ast %d\n", http.StatusOK, w.Code)
-    } else {
-        t.Fatalf("Expected to get status %d but instead got %d\n", http.StatusOK, w.Code)
-    }
+	// Check to see if the response was what you expected
+	if w.Code == http.StatusOK {
+		t.Logf("Expected to get status %d is same ast %d\n", http.StatusOK, w.Code)
+	} else {
+		t.Fatalf("Expected to get status %d but instead got %d\n", http.StatusOK, w.Code)
+	}
 }
 
 func TestPutMethod(t *testing.T) {
-    // Switch to test mode so you don't get such noisy output
-    gin.SetMode(gin.TestMode)
+	// Switch to test mode so you don't get such noisy output
+	gin.SetMode(gin.TestMode)
 
-    // Setup your router, just like you did in your main function, and
-    // register your routes
-    r := gin.Default()
-    r.PUT("/", PutMethod)
+	// Setup your router, just like you did in your main function, and
+	// register your routes
+	r := gin.Default()
+	r.PUT("/", PutMethod)
 
-    // Create the mock request you'd like to test. Make sure the second argument
-    // here is the same as one of the routes you defined in the router setup
-    // block!
-    req, err := http.NewRequest(http.MethodPut, "/", nil)
-    if err != nil {
-        t.Fatalf("Couldn't create request: %v\n", err)
-    }
+	// Create the mock request you'd like to test. Make sure the second argument
+	// here is the same as one of the routes you defined in the router setup
+	// block!
+	req, err := http.NewRequest(http.MethodPut, "/", nil)
+	if err != nil {
+		t.Fatalf("Couldn't create request: %v\n", err)
+	}
 
-    // Create a response recorder so you can inspect the response
-    w := httptest.NewRecorder()
+	// Create a response recorder so you can inspect the response
+	w := httptest.NewRecorder()
 
-    // Perform the request
-    r.ServeHTTP(w, req)
-    fmt.Println(w.Body)
+	// Perform the request
+	r.ServeHTTP(w, req)
+	fmt.Println(w.Body)
 
-    // Check to see if the response was what you expected
-    if w.Code == http.StatusOK {
-        t.Logf("Expected to get status %d is same ast %d\n", http.StatusOK, w.Code)
-    } else {
-        t.Fatalf("Expected to get status %d but instead got %d\n", http.StatusOK, w.Code)
-    }
+	// Check to see if the response was what you expected
+	if w.Code == http.StatusOK {
+		t.Logf("Expected to get status %d is same ast %d\n", http.StatusOK, w.Code)
+	} else {
+		t.Fatalf("Expected to get status %d but instead got %d\n", http.StatusOK, w.Code)
+	}
 }
